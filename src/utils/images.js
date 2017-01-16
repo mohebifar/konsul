1dcdf84d87af7a97b2f61ef70de7d229ac1b3524
Mohamad Mohebifar <netfars@gmail.com>
2017-01-15 21:33:37 -0330

    feat(utils/images): CalculateImageDimensions

---
+++ src/utils/images.js
@@ -0,0 +1,24 @@
+import type { Dimension } from '../types/images';
+
+export function calculateDimensions(requestedDimension: Dimension, actualDimension?: Dimension) {
+  let width = 0;
+  let height = 0;
+
+  if (requestedDimension.width && requestedDimension.height) {
+    width = requestedDimension.width;
+    height = requestedDimension.height;
+  } else if (actualDimension) {
+    if (requestedDimension.width) {
+      width = requestedDimension.width;
+      height = actualDimension.height / actualDimension.width * requestedDimension.width;
+    } else if (requestedDimension.height) {
+      width = actualDimension.width / actualDimension.height * requestedDimension.height;
+      height = requestedDimension.height;
+    } else {
+      width = actualDimension.width;
+      height = actualDimension.height;
+    }
+  }
+
+  return { width, height };
+}
