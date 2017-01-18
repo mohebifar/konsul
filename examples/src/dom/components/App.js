d4d68fcb5b174f63ea7add8b1bdf6fcba057c4c7
Mohamad Mohebifar <netfars@gmail.com>
2017-01-18 13:05:52 -0330

    doc(examples): Improve the examples code and style

--- examples/src/dom/components/App.js
+++ examples/src/dom/components/App.js
@@ -6,13 +6,19 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+        <a href="https://github.com/mohebifar/konsul">
+          <img style={{position: 'absolute', top: 0, right: 0, border: 0}}
+               src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+               alt="Fork me on GitHub"
+          />
+        </a>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to Konsul!</h2>
         </div>
 
         <div>
-          <strong>See examples:</strong>
+          <strong>See the examples:</strong>
           <ul className="App-navigation">
             <li><a href="?basic#/">Basic</a></li>
             <li><a href="?code#/code">Code Highlight</a></li>
@@ -23,9 +29,11 @@ class App extends Component {
         </div>
 
         <div className="App-intro">
-          <p>Open your browser's dev console to see what Konsul does!</p>
-            <p><code>⌘+⇧+J</code> (Mac)</p>
-            <p><code>CTRL+SHIFT+J</code> (Linux & Windows)</p>
+          Please open the console.
+          <div className="App-instruction">
+            <div><code>⌘+⇧+J</code> Mac</div>
+            <div><code>CTRL+SHIFT+J</code> Linux & Windows</div>
+          </div>
         </div>
       </div>
     );
