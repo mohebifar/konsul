a1ef5741be736f555fe4348a476357fa681ea0c7
mohebifar <netfars@gmail.com>
2017-07-22 10:20:50 -0230

    fix: Pass the factory options to konsul constructor

--- packages/konsul/src/index.js
+++ packages/konsul/src/index.js
@@ -1,9 +1,8 @@
 import Konsul from './Konsul';
-export Text from './widgets/Text';
 import registerDefaultWidgets from './registerDefaultWidgets';
 
 export default function create (options) {
-  const konsul = new Konsul();
+  const konsul = new Konsul(options);
   registerDefaultWidgets(konsul);
   return konsul;
 }
